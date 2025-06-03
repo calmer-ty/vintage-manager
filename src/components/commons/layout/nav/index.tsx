a6e315af7705e1bc796d8565d5c4bbcdff24b3a8
Calmer <ty.calmer96@gmail.com>
2025-06-03 20:47:54 +0900

    refactor: 메뉴 아이템 컴포넌트화

--- src/components/commons/layout/nav/index.tsx
+++ src/components/commons/layout/nav/index.tsx
@@ -1,30 +1,37 @@
 import { memo } from "react";
 
-// import Link from "next/link";
-
 import { motion } from "framer-motion";
-import Image from "next/image";
 
-// import * as S from "./styles";
-
-const mapsMenus = [
-  { type: "apartment", title: "대시보드", icon: "icon_table" },
-  { type: "officetel", title: "상품등록", icon: "icon_plus" },
+interface IMenu {
+  title: string;
+  icon: string;
+}
+const mapsMenus: IMenu[] = [
+  { title: "대시보드", icon: "icon_table" },
+  { title: "상품등록", icon: "icon_plus" },
 ];
 
 function MapSelector() {
   return (
-    <div className="flex flex-col gap-8 w-30 h-screen bg-[#344955] pt-8">
-      {mapsMenus.map((menu, index) => (
-        <motion.div key={index} whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} className="flex flex-col justify-center items-center gap-2 text-white cursor-pointer">
-          {/* <Link key={index} href={`/${menu.type}`} className={menu.type === buildingType ? "active" : ""}> */}
-          {/* <>{menu.icon}</> */}
-          <Image alt={menu.icon} width={32} height={32} src={`/images/${menu.icon}.svg`} />
-          <span>{menu.title}</span>
-          {/* </Link> */}
-        </motion.div>
+    <div className="flex flex-col items-center gap-8 w-30 h-screen bg-[#4A4E69] pt-8">
+      {mapsMenus.map((menu) => (
+        <MenuItem key={menu.title} menu={menu} />
       ))}
     </div>
+  );
+}
+
+function MenuItem({ menu }: { menu: IMenu }) {
+  return (
+    <motion.div whileHover={{ backgroundColor: "#5B637E" }} transition={{ duration: 0.2 }} className="flex flex-col items-center gap-2 p-4 cursor-pointer rounded-lg">
+      <i
+        className="block w-8 h-8 bg-no-repeat bg-contain"
+        style={{
+          backgroundImage: `url(/images/${menu.icon}.svg)`,
+        }}
+      />
+      <span className="text-white">{menu.title}</span>
+    </motion.div>
   );
 }
 
