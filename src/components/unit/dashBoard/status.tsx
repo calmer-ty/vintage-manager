5664baa8eb6b627d405c2efa09e8daff182382e6
Calmer <ty.calmer96@gmail.com>
2025-07-25 11:43:38 +0900

    fix: 총 매입은 items에서 데이터 추출해야함, 수정 완료

--- src/components/unit/dashBoard/status.tsx
+++ src/components/unit/dashBoard/status.tsx
@@ -4,16 +4,10 @@ import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { DollarSign, TrendingUp } from "lucide-react";
 import { IItemData } from "@/types";
 
-export default function DashBoardStatus({ itemData }: { itemData: IItemData[] }) {
-  // const soldItemCostPrices = soldItems.map((item) => item.costPriceKRW);
-  // const totalSoldItemCostPrices = soldItemCostPrices.reduce((acc, cur) => {
-  //   console.log("cur: ", cur);
-  //   console.log("cur-2: ", cur.costPriceKRW);
-  //   console.log("acc: ", acc);
-  //   return acc + cur;
-  // }, 0);
-  const soldItems = itemData.filter((item) => item.soldAt !== null);
-
+export default function DashBoardStatus({ items }: { items: IItemData[] }) {
+  // soldAt null이 아닌 item 데이터들
+  const soldItems = items.filter((item) => item.soldAt !== null);
+  // items의 특정 키의 값들을 모두 더하여 합한 값을 계산하는 함수
   function sumField(items: IItemData[], field: keyof IItemData) {
     let sum = 0;
 
@@ -21,17 +15,18 @@ export default function DashBoardStatus({ itemData }: { itemData: IItemData[] })
       const value = item[field];
 
       // undefined나 null일 경우 0으로 처리
-      if (value !== undefined && value !== null) {
+      if (value != null) {
         sum += Number(value);
       }
     }
 
     return sum;
   }
-  const totalCostPriceKRW = sumField(soldItems, "costPriceKRW");
+  const totalCostPriceKRW = sumField(items, "costPriceKRW");
   const totalSalePrice = sumField(soldItems, "salePrice");
   const totalProfit = sumField(soldItems, "profit");
 
+  // 상단의 상태 값들
   const infoStatus = [
     {
       title: "총 매입",
