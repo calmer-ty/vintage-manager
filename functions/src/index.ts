284a8751c8a68e395032cffc710941df59a394b2
Calmer <ty.calmer96@gmail.com>
2025-09-23 15:49:20 +0900

    chore: functions v2 버전으로 업그레이드

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,24 +5,19 @@ import next from "next";
 // Firebase Admin 초기화
 admin.initializeApp();
 
-const isDev = process.env.NODE_ENV !== "production";
-const nextServer = next({
-  dev: isDev,
-  conf: {distDir: ".next"},
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev, conf: { distDir: ".next" } });
+
+const handle = app.getRequestHandler();
+
+export const nextjsFunc = functions.https.onRequest({ region: "asia-northeast3" }, async (req, res) => {
+  try {
+    // Next.js 준비
+    await app.prepare();
+
+    return handle(req, res);
+  } catch (error) {
+    console.error("Error handling the request:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
-
-const handle = nextServer.getRequestHandler();
-
-export const nextjsFunc = functions
-  .region("asia-northeast1") // 리전 설정 (v1에서는 각 함수마다 리전 설정)
-  .https.onRequest(async (req, res) => {
-    try {
-      // Next.js 준비
-      await nextServer.prepare();
-
-      return handle(req, res);
-    } catch (error) {
-      console.error("Error handling the request:", error);
-      res.status(500).send("Internal Server Error");
-    }
-  });
