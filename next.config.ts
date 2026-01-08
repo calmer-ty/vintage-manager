20dbaa5f5d3ae4ffadbf3e06532ff1b61983a59e
Calmer <ty.calmer96@gmail.com>
2026-01-08 10:47:03 +0900

    chore: 키값 재설정 및 리액트 업뎃

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // distDir: "functions/.next", // ✅ 빌드 결과를 functions 폴더 안으로 이동
-  // env: {
-  //   FIREBASE_APIKEY: process.env.FIREBASE_APIKEY,
-  //   FIREBASE_AUTHDOMAIN: process.env.FIREBASE_AUTHDOMAIN,
-  //   FIREBASE_PROJECTID: process.env.FIREBASE_PROJECTID,
-  //   FIREBASE_STORAGEBUCKET: process.env.FIREBASE_STORAGEBUCKET,
-  //   FIREBASE_MESSAGINGSENDERID: process.env.FIREBASE_MESSAGINGSENDERID,
-  //   FIREBASE_APPID: process.env.FIREBASE_APPID,
-  //   FIREBASE_MEASUREMENTID: process.env.FIREBASE_MEASUREMENTID,
-  //   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  //   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  //   EXCHANGERATE_API_KEY: process.env.EXCHANGERATE_API_KEY,
-  // },
 };
 
 export default nextConfig;
--- src/lib/firebase/firebaseApp.ts
+++ src/lib/firebase/firebaseApp.ts
@@ -5,13 +5,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: process.env.FIREBASE_AUTHDOMAIN,
-  projectId: process.env.FIREBASE_PROJECTID,
-  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
-  appId: process.env.FIREBASE_APPID,
-  measurementId: process.env.FIREBASE_MEASUREMENTID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
